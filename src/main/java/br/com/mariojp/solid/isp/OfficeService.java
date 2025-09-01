25efa6998fb3078cb37fd9307edd2e749b9333c4
Isadora Carvalho <isadora.pereira@ucsal.edu.br>
2025-09-01 18:50:47 -0300

    Garante que apenas IPrint seja usada

--- src/main/java/br/com/mariojp/solid/isp/OfficeService.java
+++ src/main/java/br/com/mariojp/solid/isp/OfficeService.java
@@ -1,11 +1,11 @@
 package br.com.mariojp.solid.isp;
 
 public class OfficeService {
-    private final MultiFunctionDevice device;
-    public OfficeService(MultiFunctionDevice device){ this.device = device; }
+    private final IPrint device;
+    public OfficeService(IPrint device){ this.device = device; }
     public void printReport(String content){
         device.print(content);
         // BUG: chama scan sem necessidade
-        device.scan("audit.txt");
+        // device.scan("audit.txt");
     }
 }
